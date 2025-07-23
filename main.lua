828b2626ce0cfe5b3f49474dd18e950b16ea351e
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-07-23 20:19:31 -0300

    Upload do main

---
+++ main.lua
@@ -0,0 +1,25 @@
+--
+-- Author: Matheus R. Oliveira
+-- Github: matheusoreis
+--
+
+function love.load()
+end
+
+function love.update()
+end
+
+function love.draw()
+end
+
+function love.keypressed(key)
+end
+
+function love.keyreleased(key)
+end
+
+function love.gamepadpressed(_, button)
+end
+
+function love.gamepadreleased(_, button)
+end
